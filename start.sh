d7b125ef8a2f29d3ebef8cc64f73b97a5d52ffa4
lllnnll <nikitasitchevoy@gmail.com>
2021-07-29 23:17:36 +0200

    oe

---
+++ start.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+DOCKER_NAME="yubot"
+
+echo remove docker container
+docker rm -f $DOCKER_NAME
+echo remove docker image
+docker rmi -f $DOCKER_NAME
+
+echo build docker image
+docker build -t $DOCKER_NAME .
+echo run docker container
+docker run -d \
+    --name $DOCKER_NAME \
+    $DOCKER_NAME
