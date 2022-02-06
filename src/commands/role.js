65f4536ebc29c3527249fa3006e012f8a86a2a11
lllnnll <nikitasitchevoy@gmail.com>
2022-02-06 20:12:14 +0100

    oe

--- src/commands/role.js
+++ src/commands/role.js
@@ -5,17 +5,23 @@ const commands = {
     category: 'administration',
     handle: async (client,message,args) => {
         message.channel.send(`Voici les roles :`);
-        msgBot = await message.channel.send('Valorant LoL CS:GO Genshin ');
+        msgBot = await message.channel.send('Valorant / LoL / CS:GO / Genshin / Forza / RL');
         msgBot.react('😮');
         msgBot.react('🚿');
         msgBot.react('😎');
         msgBot.react('🍆');
+        msgBot.react('🚗');
+        msgBot.react('⚽');
         client.reactionrole.set(msgBot.id, {
             messageid:msgBot.id,
             reactions: {'😮':'Valorant',
             '🚿':'LoL',
             '😎':'CS:GO',
-            '🍆':'Genshin'}
+            '🍆':'Genshin'
+            ,'🚗':'Forza'
+            ,'⚽':'RL'
+        }
+
         })
     }
 }; 
